3893380ed03912fcf8f67d732584e9675533a9ee
Suchak Barik <suchak@gmail.com>
2024-01-05 15:35:20 +0530

    Remove unwanted code

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,7 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   def image_url
-    Rails.application.routes.url_helpers.url_for(image) if image.attached?
+    Rails.application.routes.url_helpers.rails_blob_path(image) if image.attached?
   end
 
   # has_one_attached :image do |attachable|
--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,20 +1,4 @@
 class ItemSerializer
   include JSONAPI::Serializer
-  attributes :title, :list_id, :description, :image, :image_url
-
-  def image_url
-    Rails.application.routes.url_helpers.url_for(image) if image.attached?
-  end
-
-  # def image
-  #   return unless @item.image.attached?
-
-  #   Rails.application.routes.url_helpers.rails_blob_path(@item.image, only_path: true)
-  # end
-
-  # def image
-  #   return unless @item.image.attached?
-
-  #   url_for(item.image)
-  # end
+  attributes :title, :list_id, :description, :image_url
 end
